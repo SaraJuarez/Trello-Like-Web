57a89b5aef4ebd2d842441ef4c96675b4cefe366
adalaber <sarajuarezzarco@gmail.com>
2020-05-07 13:23:05 +0200

    Add paint api card functionality

---
+++ _src/assets/js/api.js
@@ -0,0 +1,30 @@
+'use strict';
+import { createHtmlList, paintList } from './list.js';
+import { createHtmlCard } from './card.js';
+import { paintCard } from './card.js';
+
+
+let info = {};
+let infoArray = new Array();
+
+function getDataFromApi() {
+    fetch('../../api/board.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.board.list.length; i++) {
+                const info = data.board.list[i];
+                infoArray.push(info)
+            }
+            createHtmlList();
+            createHtmlCard();
+            paintList();
+            paintCard();
+
+        })
+}
+
+getDataFromApi();
+
+export { infoArray };
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,4 +1,7 @@
 'use strict';
-
+import './api.js';
 import './edit.js';
 import './menu.js';
+import './list.js';
+import './card.js';
+
